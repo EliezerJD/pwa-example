2727654ba3423e551d310d265b17c1b7fc197da4
EliezerJD <163173@ids.upchiapas.edu.mx>
2019-12-10 01:48:48 -0600

    update4

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,7 +43,22 @@ export default {
       email: '',
       password: ''
     }
-  .message);
+  },
+  methods: {
+    login: function() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          alert("Logueado");
+          localStorage.setItem("id", firebase.auth().currentUser.uid);
+          console.log(localStorage.getItem("token"));
+          firebase.database().ref("tokens_nav/"+localStorage.getItem("id")).set({
+                token: localStorage.getItem("token"),
+          });
+          this.$router.replace('home')
+        },
+        (error) => {
+          alert("Error");
+          console.log(error.message);
         }
       );
     },
